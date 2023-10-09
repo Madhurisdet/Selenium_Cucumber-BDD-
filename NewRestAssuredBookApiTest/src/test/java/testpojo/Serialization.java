f9f9f3df36adf3b45c954501585191748c15de47
Madhurisdet27 <madhurikhatke27@gmail.com>
2023-10-09 00:13:06 -0700

    commit first RestAssuredAutomationAPI

---
+++ NewRestAssuredBookApiTest/src/test/java/testpojo/Serialization.java
@@ -0,0 +1,24 @@
+package testpojo;
+
+
+	
+		import java.io.FileOutputStream;
+		import java.io.ObjectOutputStream;
+		import java.io.IOException;
+
+		public class Serialization {
+		    public static void main(String[] args) {
+		        Student student = new Student(null, 0);
+		        student.setName("Madhuri");
+		        student.setAge(28);
+
+		       
+
+		           
+		      
+		}
+
+
+	}
+
+
